0fc237985bb1fe49e9efdda3ad226e7ad3ab63eb
K-Holmes <Kayla.Holmes@cdpipelinedevshop.com>
2022-07-27 17:11:10 +0000

    Added Ratings

--- fbclone/src/App.js
+++ fbclone/src/App.js
@@ -1,10 +1,14 @@
 import Album from './Components/Album'
 import './App.css';
+import Rate from './Components/Rate'
+import Circles from './Components/Circles'
 
 function App() {
   return (
     <div>
       <Album></Album>
+      <Rate></Rate>
+      <Circles></Circles>
     </div>
   );
 }
